13c55bf7815069a7e84594c260a76a8f9b5d8427
Jc Miñarro <josecarlos.minarro@gmail.com>
2026-02-03 21:53:00 +0100

    feat(hero): wire external heros list into index and add src/data/heros.js

---
+++ src/data/heros.js
@@ -0,0 +1,14 @@
+export const heros = {
+  proyectos: [
+    { url: 'https://videos.pexels.com/video-files/7578542/7578542-uhd_3840_2160_30fps.mp4', type: 'video/mp4' }
+  ],
+  promotores: [
+    { url: 'https://videos.pexels.com/video-files/7646697/7646697-uhd_2560_1440_25fps.mp4', type: 'video/mp4' }
+  ],
+  inversores: [
+    { url: 'https://videos.pexels.com/video-files/3209211/3209211-uhd_3840_2160_25fps.mp4', type: 'video/mp4' }
+  ],
+  alfaHunters: [
+    { url: 'https://videos.pexels.com/video-files/30339655/13005021_640_360_30fps.mp4', type: 'video/mp4' }
+  ]
+};
